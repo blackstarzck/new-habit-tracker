e4fe1e48638780e58b9f696a87f33b9c0c271f4a
chanki <bucheongosok@gmail.com>
2022-07-14 17:54:24 +0900

    완료

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,28 +1,47 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react' // PureComponent와 같다.
 
-export class HabitAddForm extends PureComponent {
-    formRef = React.createRef();
-    inputRef = React.createRef();
+const HabitAddForm = memo(props => {
+    const formRef = React.createRef();
+    const inputRef = React.createRef();
 
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-        const name = this.inputRef.current.value;
-        name && this.props.onAdd(name);
+        const name = inputRef.current.value;
+        name && props.onAdd(name);
 
-        // this.inputRef.current.value = "";
-        console.log(this.formRef)
-        this.formRef.current.reset();
+        // inputRef.current.value = "";
+        formRef.current.reset();
     }
+    console.log('HabitAddForm');
 
-    render() {
-        console.log("habitAddForm")
-        return (
-            <form className='form' onSubmit={this.onSubmit}>
-                <input ref={this.inputRef} className='add-input' type="text" placeholder='Habit' />
-                <button className="add-button">Add</button>
-            </form>
-        )
-    }
-}
+    return (
+        <form ref={formRef} className='form' onSubmit={onSubmit}>
+            <input ref={inputRef} className='add-input' type="text" placeholder='Habit' />
+            <button className="add-button">Add</button>
+        </form>
+    )
+});
+
+// function HabitAddForm (props) {
+//     const formRef = React.createRef();
+//     const inputRef = React.createRef();
+
+//     const onSubmit = (e) => {
+//         e.preventDefault();
+//         const name = inputRef.current.value;
+//         name && props.onAdd(name);
+
+//         // inputRef.current.value = "";
+//         formRef.current.reset();
+//     }
+//     console.log('HabitAddForm');
+
+//     return (
+//         <form ref={formRef} className='form' onSubmit={onSubmit}>
+//             <input ref={inputRef} className='add-input' type="text" placeholder='Habit' />
+//             <button className="add-button">Add</button>
+//         </form>
+//     )
+// }
 
 export default HabitAddForm
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js'
+import SimpleHabit from './components/simpleHabit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SimpleHabit/>
   </React.StrictMode>
 );
